6de15271ee35dd58246f5e7750d12abca4c88f9c
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-03-27 17:00:49 +0200

    cpmmoit

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,14 +1,14 @@
-import Contact from '../Contact/Contact';
-import css from './ContactList.module.css';
-import { useSelector } from 'react-redux';
-import { selectFilteredContacts } from '../../redux/contactsSlice';
+import Contact from "../Contact/Contact";
+import css from "./ContactList.module.css";
+import { useSelector } from "react-redux";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 
 export default function ContactList() {
   const contacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
-      {contacts.map(contact => (
+      {contacts.map((contact) => (
         <li key={contact.id} className={css.item}>
           <Contact contact={contact} />
         </li>
@@ -16,3 +16,5 @@ export default function ContactList() {
     </ul>
   );
 }
+
+//////
