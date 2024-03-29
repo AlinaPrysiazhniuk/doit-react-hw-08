bedc08a963bb99bb460186f6b20bc8a67ad0f9ba
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-03-29 18:13:49 +0200

    loading

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -5,13 +5,14 @@ import { useEffect } from "react";
 import { fetchContacts } from "../../redux/contacts/contactsOps";
 import { selectLoading } from "../../redux/contacts/selectors";
 import Loader from "../../components/Loader/Loader";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import SearchBox from "../../components/SearchBox/SearchBox";
+import Error from "../../components/Error/Error";
+import { selectError } from "../../redux/contacts/selectors";
 
 export default function Contacts() {
   const dispatch = useDispatch();
   const loading = useSelector(selectLoading);
-  // const error = useSelector(selectError);
-  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const error = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -19,14 +20,12 @@ export default function Contacts() {
 
   return (
     <>
-      {isLoggedIn && (
-        <>
-          <h2>Phonebook</h2>
-          <ContactForm />
-        </>
-      )}
+      <h2>Phonebook</h2>
+      <ContactForm />
       {loading && <Loader />}
-      {isLoggedIn && <ContactList />}
+      <SearchBox />
+
+      {error ? <Error>Error! </Error> : <ContactList />}
     </>
   );
 }
