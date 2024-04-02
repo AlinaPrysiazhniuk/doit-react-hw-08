918d4d630b90f49d53e6633345097686f84e2a52
alina prysiazhniuk <alinkaprisiazhnuyk@gmail.com>
2024-04-02 11:52:29 +0300

    private route

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,5 +1,8 @@
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import {
+  selectIsLoggedIn,
+  selectIsRefreshing,
+} from "../../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
 
 export const PrivateRoute = ({
@@ -7,5 +10,10 @@ export const PrivateRoute = ({
   redirectTo = "/login",
 }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
-  return isLoggedIn ? Component : <Navigate to={redirectTo} />;
+  const isRefreshing = useSelector(selectIsRefreshing);
+  return !isLoggedIn && !isRefreshing ? (
+    <Navigate to={redirectTo} />
+  ) : (
+    Component
+  );
 };
